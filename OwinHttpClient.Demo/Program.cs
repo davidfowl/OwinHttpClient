0094415781bd06d925501b4aad2fd48cd3b5634e
David Fowler <davidfowl@gmail.com>
2013-02-11 18:38:54 -0800

    Added sample for following redirects.

--- OwinHttpClient.Demo/Program.cs
+++ OwinHttpClient.Demo/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Owin
@@ -11,20 +9,25 @@ namespace Owin
     {
         static void Main(string[] args)
         {
-            MakeRequest().Wait();
+            MakeRequest(200).Wait();
+            FollowRedirects(5).Wait();
         }
 
-        private static async Task MakeRequest()
+        private static async Task MakeRequest(int statusCode)
         {
             var client = new OwinHttpClient();
-            var env = await client.Get("http://httpbin.org/status/418");
-
+            var env = await client.Get("http://httpbin.org/status/" + statusCode);
 
             Console.WriteLine("========");
             Console.WriteLine("Request");
             Console.WriteLine("========");
             Console.WriteLine(env[OwinHttpClientConstants.HttpClientRawRequest]);
 
+            await PrintResponse(env);
+        }
+
+        private static async Task PrintResponse(IDictionary<string, object> env)
+        {
             Console.WriteLine("========");
             Console.WriteLine("Response");
             Console.WriteLine("========");
@@ -32,6 +35,38 @@ namespace Owin
 
             var reader = new StreamReader((Stream)env["owin.ResponseBody"]);
             Console.WriteLine(await reader.ReadToEndAsync());
+        }
+
+        private static async Task FollowRedirects(int n)
+        {
+            var client = new OwinHttpClient();
+
+            string url = "http://httpbin.org/redirect/" + n;
+
+            IDictionary<string, object> env;
+
+            while (true)
+            {
+                env = await client.Get(url);
+                
+                Console.WriteLine("========");
+                Console.WriteLine("Request");
+                Console.WriteLine("========");
+                Console.WriteLine(env[OwinHttpClientConstants.HttpClientRawRequest]);
+
+                await PrintResponse(env);
+
+                var statusCode = (int)env["owin.ResponseStatusCode"];
+                if (statusCode == 302 || statusCode == 301)
+                {
+                    var headers = (IDictionary<string, string[]>)env["owin.ResponseHeaders"];
+                    url = headers["Location"][0];
+                }
+                else
+                {
+                    break;
+                }
+            }            
         }
     }
 }

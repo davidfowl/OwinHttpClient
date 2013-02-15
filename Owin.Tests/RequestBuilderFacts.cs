a0b3d43970e1e90dd6eb0b3f7dbc613ac8934231
rlbisbe <roberto.luis@rlbisbe.net>
2013-02-15 01:13:59 +0100

    First project with XUnit and a first commit message

---
+++ Owin.Tests/RequestBuilderFacts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+using Owin.Client;
+using System.Collections.Concurrent;
+using System.IO;
+
+namespace Owin.Tests
+{
+    public class RequestBuilderFacts
+    {
+        [Fact]
+        public void BuildRequestFromUriTest()
+        {
+            var requestHeaders = new ConcurrentDictionary<string, string[]>();
+            requestHeaders["Host"] = new string[] { "localhost:80" };
+            var expected = new ConcurrentDictionary<string, object>();
+            expected["owin.RequestHeaders"] = requestHeaders;
+            expected["owin.ResponseBody"] = new MemoryStream();
+            expected["owin.RequestProtocol"] = "HTTP/1.1";
+            expected["owin.ResponseHeaders"] = new ConcurrentDictionary<string, string[]>();
+            expected["owin.RequestQueryString"] = "";
+            expected["owin.RequestScheme"] = "http";
+            expected["owin.RequestPath"] = "/";
+            expected["owin.RequestPathBase"] = "";
+            expected["owin.RequestMethod"] = "GET";
+
+            var actual = RequestBuilder.Get("http://localhost/");
+
+            Assert.Equal(expected["owin.RequestProtocol"],
+                actual["owin.RequestProtocol"]);
+
+            Assert.Equal(expected["owin.RequestQueryString"],
+                actual["owin.RequestQueryString"]);
+
+            Assert.Equal(expected["owin.RequestScheme"],
+                actual["owin.RequestScheme"]);
+
+            Assert.Equal(expected["owin.RequestPath"],
+                actual["owin.RequestPath"]);
+
+            Assert.Equal(expected["owin.RequestPathBase"],
+                actual["owin.RequestPathBase"]);
+
+            Assert.Equal(expected["owin.RequestMethod"],
+                actual["owin.RequestMethod"]);
+        }
+    }
+}

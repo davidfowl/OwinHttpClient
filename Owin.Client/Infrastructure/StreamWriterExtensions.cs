e64beab600c1b8114077164ae43d369bd58fd2a5
Dale Ragan <dale@ragan.io>
2013-09-06 22:06:52 -0400

    Doh, style fix.

--- Owin.Client/Infrastructure/StreamWriterExtensions.cs
+++ Owin.Client/Infrastructure/StreamWriterExtensions.cs
@@ -4,21 +4,21 @@ namespace Owin.Client.Infrastructure
 {
     public static class StreamWriterExtensions
     {
-        static string HTTP_NEW_LINE = "\r\n";
+        static string HttpNewLine = "\r\n";
 
         public static void WriteHttpLine(this StreamWriter writer)
         {
-            writer.Write(HTTP_NEW_LINE);
+            writer.Write(HttpNewLine);
         }
 
         public static void WriteHttpLine(this StreamWriter writer, string format, object arg0, object arg1)
         {
-            writer.Write(format + HTTP_NEW_LINE, arg0, arg1);
+            writer.Write(format + HttpNewLine, arg0, arg1);
         }
 
         public static void WriteHttpLine(this StreamWriter writer, string format, object arg0, object arg1, object arg2)
         {
-            writer.Write(format + HTTP_NEW_LINE, arg0, arg1, arg2);
+            writer.Write(format + HttpNewLine, arg0, arg1, arg2);
         }
     }
 }
